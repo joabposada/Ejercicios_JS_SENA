2c30fdb1310e453f62c16f78c07ae91ae8719a33
Joab <joabposada@gmail.com>
2026-04-02 19:23:21 -0500

    Ejercicio del 6 al 11 completado

--- main.js
+++ main.js
@@ -72,9 +72,118 @@ function numeroAleatorio(){
 //formula: fn=fn-1 +fn-2
 function Fibonacci(){
     let cantidad = Number(document.getElementById("numeroInicial").value);
-    let serie = [0,1] 
+    let serie = [0,1] //numero ocupados a iniciar
+
+for (let i = 2; i< cantidad; i++){ // i es un contador y vale a 2
+
+
+let nuevoNum = serie [i - 1] + serie [i - 2] // siempre dos pasos atras, o sea los busca
+serie.push(nuevoNum);
+}
+document.getElementById("resultado").innerText = serie;
+
+ }
+
+ //Ejercicio 7: Mayor o menor
+
+ function mayoromenor(){
+    let num1= Number(document.getElementById("Numero1").value);
+    let num2= Number(document.getElementById("Numero2").value);
+     if (num1 > num2) {
+        document.getElementById("Resultado").innerText = "El primer numero es mayor " 
+    }else if (num1<num2){
+        document.getElementById("Resultado").innerText = "El primer numero  es menor "
+    }else {
+        document.getElementById("Resultado").innerText = "son iguales "
+    }
 }
 
+
+//Ejercicio 8: Factorial
+
+function factorial(){
+    let numero =Number(document.getElementById("numeroFactor").value)
+    let resultado = 1 //en multiplicacion nuca usamos 0
+    for (let i = 1; i<= numero; i++){
+        resultado = resultado * i;
+
+    }
+    document.getElementById("respuesta").innerText = resultado;
+}
+
+//Ejercicio 9: Multiplos de 6
+
+function multiplo(){
+    let num1 = Number(document.getElementById("primerNum").value);
+    let num2 = Number(document.getElementById("segundoNum").value);
+    let listaMultiplos= [];
+     if (num1 > num2) {
+        console.log ("Por favor un numero menor primero para hacer la operacion correctamente")
+        return;
+     }
+    for (let i = num1; i<= num2; i++){
+        if ( i % 6=== 0) {
+            listaMultiplos.push(i)
+
+        }
+        
+
+    }
+        
+
+    
+    document.getElementById("Respuesta").innerText = listaMultiplos;
+}
+
+//ejercicio 10: Par e impar
+
+function pareimpar(){
+    let elegidos = [];
+    let pares = [];
+    let impares = [];
+    for(let i=0; i<20; i++){
+         
+        let numerosAzar = Math.floor(Math.random() * 101);
+        elegidos.push(numerosAzar)
+        if (numerosAzar%2===0){
+            pares.push(numerosAzar);
+        
+
+        }else {
+            impares.push(numerosAzar);
+            
+        }
+
+        
+ }
+ document.getElementById("numerosElegidos").innerText = elegidos;
+ document.getElementById("par").innerText = "Los pares son: " + pares;
+ document.getElementById("impar").innerText = "Los impares son: "+ impares;
+}
+    //ejercicio 11: multiplos de 3 fuera
+    function multiplode3(){
+        let originales = [];
+        let eliminados = [];
+        for (let i=0; i<10; i++){
+            let numerosAzarsegundo = Math.floor(Math.random() * 11) + 10; 
+            
+            originales.push(numerosAzarsegundo); //tener los 10 antes de elminar
+             //primero el arreglo y luego el numero
+        
+
+        if (numerosAzarsegundo %3 !==0){ //! es negacion
+            eliminados.push(numerosAzarsegundo);
+        
+            
+        }
+    
+
+
+
+        }
+    document.getElementById("antes").innerText = "Asi es originalmente: " + originales;
+    document.getElementById("resultadoFinal").innerText = "modificado: " + eliminados;
+    }
 
 
 
