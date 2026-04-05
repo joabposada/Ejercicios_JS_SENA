9760606414666e96395eb8bbb95c3424440590a0
Joab <joabposada@gmail.com>
2026-04-05 03:47:56 -0500

    Todos los ejercicios completados

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ function multiplo(){
     let num2 = Number(document.getElementById("segundoNum").value);
     let listaMultiplos= [];
      if (num1 > num2) {
-        console.log ("Por favor un numero menor primero para hacer la operacion correctamente")
+        console.log ("Por favor un numero menor primero para hacer la operacion correctamente");
         return;
      }
     for (let i = num1; i<= num2; i++){
@@ -185,5 +185,90 @@ function pareimpar(){
     document.getElementById("resultadoFinal").innerText = "modificado: " + eliminados;
     }
 
+//Ejercicio 12: Triangulos
+function Triangulos(){
+    let lado1 = parseFloat(document.getElementById("Lado1").value) //Numeros reales
+    let lado2 = parseFloat(document.getElementById("Lado2").value)
+    let lado3 = parseFloat(document.getElementById("Lado3").value)
+    //Al no saber base y altura, usamos formula de heron
+    //Basicamente sumamos los 3 lados y dividimos entre 3 (el semiperimetro)
+    let semiperimetro = (lado1 + lado2 + lado3) /2;
+    //raiz cuadrada basada en el semiperimetro y lados
+    let area = Math.sqrt(semiperimetro * (semiperimetro - lado1)* (semiperimetro - lado2) * (semiperimetro - lado3)); //Saca raiz cuadrada
+    if (lado1 === lado2 && lado2 === lado3){ //&& es y
+        document.getElementById("Equilatero").innerText = "el area del equilatero es " + area;
+        
+
+    }else if (lado1 === lado2 || lado1 === lado3 || lado2===lado3){ // || es O
+        
+        document.getElementById("Isoceles").innerText = "el area del Isoceles es " + area;
+
+    }else{
+        document.getElementById("Escaleno").innerText = "el area del escaleno es " + area;
+        
+    } 
+    
+    
+}
+
+//Ejercicio 13: Carcteres
+
+function Caracteres(){
+    let texto = document.getElementById("IngresarPalabra").value;
+    //Para dividir el texto usamo.Split()
+    let partes = texto.split(" ")
+    //Para saber la cantidad de letras usamos .length
+    // .map() es para volver a un arreglo nuevo
+    let longitud = partes.map(texto => texto.length);
+    let total = longitud.reduce((acumulado, numero) => acumulado + numero, 0);//reduce es reducir
+    document.getElementById("cantidad").innerText = "la longitudes son " + longitud + " Y la totalidad de caracteres son: " + total;
 
 
+}
+
+//Ejercicio 14: Arreglos
+
+function arreglos(){
+    let numerosarr = [];
+    let manual = [];
+    for(let i=0; i<10; i++){
+         
+        let numerosAlea = Math.floor(Math.random() * 101);
+        numerosarr.push(numerosAlea)
+        
+        }document.getElementById("Arreglo1").innerText = "Arreglo original es " + numerosarr;
+        //reverse voltea la lista y cambia su orden
+        //... crea una copia
+        let copiaInvertida = [...numerosarr].reverse();
+        document.getElementById("Arreglo2").innerText = "Arreglo con reverse es " + copiaInvertida;
+        for(let i=9; i>=0; i--){ //hacemos todo al reves
+            manual.push(numerosarr[i])
+            
+
+
+        }document.getElementById("Arreglo3").innerText = "Arreglo invertido manual es " + manual;
+
+
+
+}
+
+//Ejercicio 15: FINAL!
+
+function calcular(){
+    let n1 = Number(document.getElementById("numero1").value);
+    let n2 = Number(document.getElementById("numero2").value);
+    let resultadoNum;
+    // usamos .checked tipo interruptor para saber si algo es verdad o no
+    if (document.getElementById("suma").checked){
+        resultadoNum = n1 + n2;
+    }else if (document.getElementById("resta").checked){
+        resultadoNum = n1 - n2;
+    }else if (document.getElementById("multiplicacion").checked){
+        resultadoNum = n1 * n2;
+        
+    }else if (document.getElementById("division").checked){
+        resultadoNum= n1 / n2;
+        
+    }
+    document.getElementById("resultadoCalculadora").innerText = "El resultado es: " + resultadoNum;
+}
